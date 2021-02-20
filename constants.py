d422db93884caf644a75029f57873cb0fb41b563
kaylenepang <59495040+kaylenepang@users.noreply.github.com>
2021-02-19 16:35:31 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 RPE_MESSAGE = "You have to fill out RPE after training everyday if you played and/or ran. " \
+WELLNESS_MESSAGE= "You have to fill out Wellness before training everyday!." \
 OTHER_MESSAGE = "Fill this out every day you plan on being in the athletic village for any reason. " \
                 "If you do not fill this out before you arrive, YOU WILL BE SENT HOME."
 DEFAULT_MESSAGE = "Please fill out your symptom attestation if you plan on working on-site today "
