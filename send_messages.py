9e45fd33180cfe628d2d6f8d72d90a96bedb7f35
kaylenepang <59495040+kaylenepang@users.noreply.github.com>
2021-02-21 11:28:35 -0800

    Update send_messages.py

--- send_messages.py
+++ send_messages.py
@@ -6,8 +6,8 @@ from urllib.request import Request, urlopen
 def send_message(groupme_bot, form_link, msg):
     url = 'https://api.groupme.com/v3/bots/post'
     data = {
-        'bot_id': os.environ.get(DAILY_RPE_BOT),
-        'text': "{} {}".format(msg, RPE_link),
+        'bot_id': os.environ.get(groupme_bot),
+        'text': "{} {}".format(msg, form_link),
     }
     tries = 3
     for i in range(tries):
