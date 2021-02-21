43446c9e4809b63111fe9bdd279cf235f5b79457
kaylenepang <59495040+kaylenepang@users.noreply.github.com>
2021-02-21 11:30:10 -0800

    Update 10am_scheduled_messages.py

--- 10am_scheduled_messages.py
+++ 10am_scheduled_messages.py
@@ -27,27 +27,3 @@ send_message(
     RPE_MESSAGE # message
 )
 
-import os
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-
-def send_message(groupme_bot, form_link, msg):
-    url = 'https://api.groupme.com/v3/bots/post'
-    data = {
-        'bot_id': os.environ.get(DAILY_RPE_BOT),
-        'text': "{} {}".format(msg, RPE_link),
-    }
-    tries = 3
-    for i in range(tries):
-        try:
-            print("sending to bot {}".format(groupme_bot))
-            request = Request(url, urlencode(data).encode())
-            json = urlopen(request).read().decode()
-            print("success sending to bot {}".format(groupme_bot))
-            print("response: {}".format(json))
-            break
-        except Exception as e:
-            print("failed sending to bot {}".format(groupme_bot))
-            print(e)
-            
