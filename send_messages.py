4f19759146e8345ca5379cdc98c889f8b1259d44
kaylenepang <59495040+kaylenepang@users.noreply.github.com>
2021-03-19 13:03:38 -0700

    Update send_messages.py

--- send_messages.py
+++ send_messages.py
@@ -1,24 +1,26 @@
+import json
 import os
+import requests
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 
 def send_message(groupme_bot, form_link, msg):
     url = 'https://api.groupme.com/v3/bots/post'
-    data = {
-        'bot_id': os.environ.get(groupme_bot),
-        'text': "{} {}".format(msg, form_link),
-    }
-    tries = 3
+    bot_id = os.environ.get(groupme_bot)
+    message_text = "{} {}".format(msg, form_link)
+    url_with_params = "{}?bot_id={}&text={}".format(url, bot_id, message_text)
+
+    print("sending to bot {}".format(groupme_bot))
+    tries = 4
     for i in range(tries):
         try:
-            print("sending to bot {}".format(groupme_bot))
-            request = Request(url, urlencode(data).encode())
-            json = urlopen(request).read().decode()
-            print("success sending to bot {}".format(groupme_bot))
-            print("response: {}".format(json))
-            break
+            # request = Request(url, urlencode(data).encode())
+            # json = urlopen(request).read().decode()
+            response = requests.post(url_with_params)
+            if response.ok:
+                print("success sending to bot {} on try number {}".format(groupme_bot, i + 1))
+                break
+            print("failed: {}".format(response.text))
         except Exception as e:
-            print("failed sending to bot {}".format(groupme_bot))
-            print(e)
-
+            print("failed for bot {} on try number {} - error: {}".format(groupme_bot, i + 1, e))
